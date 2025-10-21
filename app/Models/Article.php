a36c64626627bd7733e929c2d179f32a7c7f6730
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-10-21 22:34:22 +0700

    add slug

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Article extends Model
 {
     use SoftDeletes;
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
