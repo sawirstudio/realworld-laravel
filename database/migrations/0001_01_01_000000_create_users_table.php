9565771e8df48deba7a41e2b9b8cf24be6aaf51c
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-10-21 23:27:33 +0700

    make these columns nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->text('bio')->nullable();
             $table->text('image')->nullable();
             $table->rememberToken();
