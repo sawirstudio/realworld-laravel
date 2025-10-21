9b100413ac6861f06f491dd995faf9e4db5066dc
Ricardo Sawir <37329575+sawirricardo@users.noreply.github.com>
2025-10-21 23:33:56 +0700

    add has api token

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use HasApiTokens;
     use SoftDeletes;
 
     /**
